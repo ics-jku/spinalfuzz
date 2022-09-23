c90b6fbe94767364812d99074e26c9bf54c5fc8c
Katharina Ruep <katharina.ruep@jku.at>
2022-09-23 14:50:17 +0200

    clean up

--- tester/src/test/python/spinal/Dummy/Dummy.v
+++
@@ -1,6 +0,0 @@
-module Dummy();
-  initial begin
-    $dumpfile("wave.vcd");
-    $dumpvars(1, Dummy);
-  end
-endmodule
--- tester/src/test/python/spinal/Pinsec/common/default.mk
+++
@@ -1,3 +0,0 @@
-VERILOG_SOURCES += $(SPINALROOT)/Pinsec.v ../common/mt48lc16m16a2.v ../common/PinsecTester_tb.v
-TOPLEVEL=PinsecTester_tb
-include ../../common/Makefile.sim
